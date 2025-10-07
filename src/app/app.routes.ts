915c4a9d5af389ae829875ec5739eddfc9309b51
Acsbeni <acsbenjamin@turr.hu>
2025-10-07 12:28:02 +0200

    Kategória, forgalom lista + form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CategoryListComponent } from './components/categories/list/list.component';
 import { TraficListComponent } from './components/trafics/list/list.component';
+import { TraficFormComponent } from './components/trafics/form/form.component';
+import { CategoryFormComponent } from './components/categories/form/form.component';
 
 export const routes: Routes = [
     {
@@ -10,6 +12,14 @@ export const routes: Routes = [
     {
         path: 'trafics',
         component: TraficListComponent
+    },
+    {
+        path: 'traficform',
+        component: TraficFormComponent
+    },
+    {
+        path:"categoryform",
+        component:CategoryFormComponent
     }
 
 ];
