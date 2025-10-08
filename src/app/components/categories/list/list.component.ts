25f29bdedf31271ef41710397b625453e0d3d0eb
Acsbeni <acsbenjamin@turr.hu>
2025-10-08 11:33:54 +0200

    update

--- src/app/components/categories/list/list.component.ts
+++ src/app/components/categories/list/list.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import axios from 'axios';
+
+interface Category{
+    id:number
+    kategoriaNev:string
+}
 
 @Component({
   selector: 'app-categorylist',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class CategoryListComponent {
 
+
+
+export class CategoryListComponent implements OnInit {
+
+  categories: Category[] =[]
+
+
+  async ngOnInit(){
+    try{
+      const response = await axios.get('http://localhost:3000/categories')
+      this.categories = response.data
+      console.log(this.categories)
+    }
+    catch(err){
+      console.log(err)
+      alert("Hiba")
+    }
+  }
 }
